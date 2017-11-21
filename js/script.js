d98315a1fa4b644d03cf9949d702fd74f715f718
SmXme <pillot.clement@gmail.com>
2017-11-21 16:15:21 +0100

    Added the script for remove a droppedForm + formated the HTML string

--- js/script.js
+++ js/script.js
@@ -23,7 +23,13 @@ $( function() {
     $( "#formu" ).droppable({
       drop: function( event, ui ) {
       	if (draginItem == "boxRadio"){
-      		$(this).append("<div class ='dropForm'><input type='text' id='dropTitle' placeholder='Insérer votre question ici'><input type='radio'><input type='text' class='answer' placeholder='Option'><i class='fa fa-plus-square' aria-hidden='true'></i><i class='fa fa-minus-square' aria-hidden='true'></i><button id='formButton' class='btn btn-primary' type='submit'>Valider</button></div>");                           
+      		$(this).append("<div class ='dropForm'><div id='idTest' class='divCrossForm'>"+
+            "<i class='fa fa-times' aria-hidden='true'></i></div>"+
+            "<input type='text' id='dropTitle' placeholder='Insérer votre question ici'><input type='radio'>"+
+            "<input type='text' class='answer' placeholder='Option'><i class='fa fa-plus-square' aria-hidden='true'>"+
+            "</i><i class='fa fa-minus-square' aria-hidden='true'></i>"+
+            "<button id='formButton' class='btn btn-primary' type='submit'>Valider</button></div>");                           
+          initClick();  
       	}
       	if (draginItem == "boxSelect"){
       		$(this).append("<select></select>");	
@@ -34,11 +40,13 @@ $( function() {
       	if (draginItem == "boxInfo"){
       		$(this).append("<input type='text' placeholder='Nom' /><input type='text' placeholder='Prénom' />");	
       	}
-
       	draginItem = "";
-
       }
     });
-
-
 });
+
+function initClick(){
+  $('.divCrossForm').click(function(){
+    $(this).parent().remove();
+  });
+}
