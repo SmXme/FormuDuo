4526a2eb22fcab14cef69a3bcb71ce5f3043b787
Unknown <renaud.hpavie@gmail.com>
2017-11-22 11:30:38 +0100

    duplication of the radio box in the div checkbox + started info div

--- js/script.js
+++ js/script.js
@@ -37,10 +37,19 @@ $( function() {
       		$(this).append("<select></select>");	
       	}
       	if (draginItem == "boxCheck"){
-      		$(this).append("<input type='checkbox'>");	
+      		$(this).append("<div id='divDropForm"+nextDivId+"' class ='dropForm'><div id='idTest"+nextDivId+"' class='divCrossForm'>"+
+            "<i class='fa fa-times' aria-hidden='true'></i></div>"+
+            "<input type='text' id='dropTitle' placeholder='Insérer votre question ici'>"+
+            "<div class='option' id='divOptions"+nextDivId+"'><input type='text' class='answer' placeholder='Option'><i id='iconAdd' class='fa fa-plus-square' aria-hidden='true'></i>"+
+            "<i id='iconDelete' class='fa fa-minus-square' aria-hidden='true'></i> </div>"+
+            "<button id='formButton' class='btn btn-primary' type='submit'>Valider</button></div>");
+            nextDivId++;	
       	}
       	if (draginItem == "boxInfo"){
-      		$(this).append("<input type='text' placeholder='Nom' /><input type='text' placeholder='Prénom' />");	
+      		$(this).append("<div id='divInfo'><h2>Informations personnelles</h2>"+
+                         "<input type='text' placeholder='Nom'>"+
+                         "<input type='text' placeholder='Prénom'>"+
+                         "<input type='email' placeholder='Email'></div>");	
       	}
       	draginItem = "";
       }
